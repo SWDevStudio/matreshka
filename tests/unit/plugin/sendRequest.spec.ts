56d85245ef64baf863fcd279a95be02d940b9899
Kotaro <swdevstudio@gmail.com>
2021-02-27 20:56:03 +0300

    added unit test for sendRequest plugin

--- tests/unit/plugin/sendRequest.spec.ts
+++ tests/unit/plugin/sendRequest.spec.ts
@@ -1,11 +1,11 @@
 import { $sendRequest } from '@/plugins/sendRequest'
 
+// TODO @Kotaro видимо тут нужно еще делать мока xhr
 describe('Проверка на неправильный ввод данных', () => {
   it('Несуществующая соц сеть', async () => {
-    // await $sendRequest({
-    //   method: 'test',
-    //   network: 'vk'
-    // })
-    expect('null').toBeNull()
+    expect(await $sendRequest({
+      method: 'test',
+      network: 'vk'
+    })).toBeNull()
   })
 })
